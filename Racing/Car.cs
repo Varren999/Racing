2cfdbd27ec96f62c754ec41fe94ff17cff4a3846
Varren999 <varren999@gmail.com>
2024-01-30 16:13:56 +0700

    class car

---
+++ Racing/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Car
+    {
+        double speed;
+        double Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        void Drive() { }
+    }
+}
